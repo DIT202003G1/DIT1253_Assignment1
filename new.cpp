e77805ffde2b4435c3a13dbf47cb9bc9bc49a0e9
Frankium <tis7bfrankie@gmail.com>
2020-05-15 13:19:52 +0800

    initial

---
+++ new.cpp
@@ -0,0 +1,34 @@
+/*
+	Project Details
+	
+	Subject: Programming Concept
+	Subject Details: DIT1253_202003_1
+	
+	Assignment Group: Individual
+	Group Member: Zhao Xuanao (20023404)
+	
+	Begain Date: (WED) 13 MAY 2020
+	
+	Lecturer:
+	Ms. Kumatha
+	kumatham@sunway.edu.my
+	016-5636585
+*/
+
+#include <iostream>	//standart console io
+#include <sstream>	//stringstream
+#include <fstream>	//file io
+#include <string>	//using string
+#include <cctype>	//using tolower for charactor types
+#include <map>		//using dictionary(maps)
+
+#include "Interface.cpp"	//Interface.cpp	
+#include "Error.cpp"		//Error.cpp
+#include "Plans.cpp"		//Plans.cpp
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
