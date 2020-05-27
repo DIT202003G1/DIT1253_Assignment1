44f3bcafc42b872289f7c200c70bfe522097119f
Frankium <tis7bfrankie@gmail.com>
2020-05-28 05:37:54 +0800

    give up and create a new version

---
+++ new2.cpp
@@ -0,0 +1,50 @@
+/*
+	Project Details
+	
+	Subject: Programming Concept
+	Subject Details: DIT1253_202003_1
+	
+	Assignment Group: Group ?
+	Group Members:
+	 - Zhao Xuanao (20023404)
+	 - Hee Kar Yan ()
+	 - Maisaa Ahmed ()
+	
+	Begain Date: (WED) 13 MAY 2020
+	
+	Lecturer:
+	Ms. Kumatha
+	kumatham@sunway.edu.my
+	016-5636585
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//PREDEFINES
+namespace predefined{
+
+}
+
+//UTILS FUNCTIONS
+namespace utils{
+
+}
+
+//IO INTERFACE
+namespace interface{
+
+}
+
+//PREDEFINED PLANS
+namespace plans{
+
+}
+
+//main
+int main(){
+	int returnVal;
+	returnVal = 0;
+	return returnVal;
+}
